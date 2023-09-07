368d4bcdf39e148507a37da5fc4295c2e88c3176
zongyanfu <zongyanfu@outlook.com>
2023-09-07 14:09:02 +0800

    d

--- lib/core/match/screens/match.dart
+++ lib/core/match/screens/match.dart
@@ -1,15 +1,11 @@
-import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sona/common/models/user.dart';
 import 'package:sona/core/match/providers/matched.dart';
-import 'package:sona/core/match/providers/setting.dart';
-import 'package:sona/core/match/screens/setting.dart';
+import 'package:sona/core/match/widgets/user_card.dart';
 
 import '../../../common/widgets/button/colored.dart';
-import '../../../firebase/sona_firebase.dart';
 import '../widgets/scroller.dart';
 
 class MatchScreen extends StatefulHookConsumerWidget {
@@ -21,8 +17,6 @@ class MatchScreen extends StatefulHookConsumerWidget {
 
 class _MatchScreenState extends ConsumerState<MatchScreen>
     with AutomaticKeepAliveClientMixin {
-  UserInfo? _current_user;
-
   late final Controller controller;
 
   void _handleCallbackEvent(ScrollDirection direction, ScrollSuccess success,
@@ -45,21 +39,36 @@ class _MatchScreenState extends ConsumerState<MatchScreen>
     super.build(context);
     return ref.watch(asyncMatchRecommendedProvider).when<Widget>(
           data: (List<UserInfo> users) {
-            _current_user ??= users.isNotEmpty ? users.first : null;
+            if (users.isEmpty) {
+              return Container(
+                alignment: Alignment.center,
+                child: const Text('No more.'),
+              );
+            }
             return Stack(
               children: [
                 Positioned.fill(
                     child: TikTokStyleFullPageScroller(
-                        contentSize: users.length,
-                        swipePositionThreshold: 0.2,
-                        // ^ the fraction of the screen needed to scroll
-                        swipeVelocityThreshold: 2000,
-                        // ^ the velocity threshold for smaller scrolls
-                        animationDuration: const Duration(milliseconds: 400),
-                        // ^ how long the animation will take
-                        controller: controller,
-                        // ^ registering our own function to listen to page changes
-                        builder: _cardBuilder)),
+                  contentSize: users.length,
+                  swipePositionThreshold: 0.2,
+                  // ^ the fraction of the screen needed to scroll
+                  swipeVelocityThreshold: 2000,
+                  // ^ the velocity threshold for smaller scrolls
+                  animationDuration: const Duration(milliseconds: 400),
+                  // ^ how long the animation will take
+                  controller: controller,
+                  // ^ registering our own function to listen to page changes
+                  builder: (context, index) => UserCard(
+                    key: ValueKey(users[index].id),
+                    user: users[index],
+                    onLike: () => ref
+                        .read(asyncMatchRecommendedProvider.notifier)
+                        .like(users[index].id),
+                    onArrow: () => ref
+                        .read(asyncMatchRecommendedProvider.notifier)
+                        .superlike(users[index].id),
+                  ),
+                )),
                 Positioned(
                   top: 0,
                   left: 0,
@@ -103,96 +112,6 @@ class _MatchScreenState extends ConsumerState<MatchScreen>
                 child: CircularProgressIndicator(strokeWidth: 2.5)),
           ),
         );
-  }
-
-  Widget _cardBuilder(BuildContext context, int index) {
-    final user = ref.read(asyncMatchRecommendedProvider).value![index];
-
-    return Container(
-      key: ValueKey(user.id),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: CachedNetworkImageProvider(user.photos.firstOrNull ?? ''),
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-            isAntiAlias: true),
-      ),
-      clipBehavior: Clip.antiAlias,
-      alignment: Alignment.bottomCenter,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 20,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 160,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('${user.name}',
-                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                      shadows: const <Shadow>[
-                        Shadow(
-                          blurRadius: 5.0,
-                          color: Color.fromARGB(10, 0, 0, 0),
-                        ),
-                      ],
-                    )),
-                Text('${user.age}',
-                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        shadows: const <Shadow>[
-                          Shadow(
-                            blurRadius: 5.0,
-                            color: Color.fromARGB(10, 0, 0, 0),
-                          ),
-                        ]))
-              ],
-            ),
-          ),
-          Positioned(
-              right: 20,
-              bottom: MediaQuery.of(context).viewInsets.bottom + 120,
-              child: Visibility(
-                visible: _current_user != null,
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                          width: 1,
-                          color: Theme.of(context)
-                              .colorScheme
-                              .secondaryContainer)),
-                  alignment: Alignment.center,
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Theme.of(context).colorScheme.primaryContainer,
-                      size: 33,
-                    ),
-                    onPressed: () {
-                      ref
-                          .read(asyncMatchRecommendedProvider.notifier)
-                          .like(_current_user!.id);
-                      controller.animateToPosition(index + 1);
-                    },
-                  ),
-                ),
-              ))
-        ],
-      ),
-    );
-  }
-
-  Future _leftSwipeHandler() async {
-    ref.read(asyncMatchRecommendedProvider.notifier).skip(_current_user!.id);
-  }
-
-  void _rightSwipeHandler() {
-    ref.read(asyncMatchRecommendedProvider.notifier).like(_current_user!.id);
   }
 
   void _showActions(UserInfo user) {
---
+++ lib/core/match/widgets/user_card.dart
@@ -0,0 +1,153 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../common/models/user.dart';
+
+class UserCard extends StatefulWidget {
+  const UserCard(
+      {super.key,
+      required this.user,
+      required this.onLike,
+      required this.onArrow});
+  final UserInfo user;
+  final void Function() onLike;
+  final void Function() onArrow;
+
+  @override
+  State<StatefulWidget> createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    print('photo count: ${widget.user.photos.length}');
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: PageView.builder(
+            controller: _pageController,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return _userInfo();
+              } else if (index == 1) {
+                return _userBio();
+              } else {
+                return _userPhoto(widget.user.photos[index - 1]);
+              }
+            },
+            itemCount: widget.user.photos.length + 1,
+          ),
+        ),
+        Positioned(
+          right: 20,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+          child: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                    width: 1,
+                    color: Theme.of(context).colorScheme.secondaryContainer)),
+            alignment: Alignment.center,
+            child: IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).colorScheme.primaryContainer,
+                size: 33,
+              ),
+              onPressed: widget.onLike,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _userInfo() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: CachedNetworkImageProvider(
+                widget.user.photos.firstOrNull ?? ''),
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+            isAntiAlias: true),
+      ),
+      clipBehavior: Clip.antiAlias,
+      alignment: Alignment.bottomCenter,
+      child: Stack(
+        children: [
+          Positioned(
+            left: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 160,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('${widget.user.name}',
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800,
+                      shadows: const <Shadow>[
+                        Shadow(
+                          blurRadius: 5.0,
+                          color: Color.fromARGB(10, 0, 0, 0),
+                        ),
+                      ],
+                    )),
+                Text('${widget.user.age}',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        shadows: const <Shadow>[
+                          Shadow(
+                            blurRadius: 5.0,
+                            color: Color.fromARGB(10, 0, 0, 0),
+                          ),
+                        ]))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _userBio() {
+    return Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: CachedNetworkImageProvider(
+                  widget.user.photos.firstOrNull ?? ''),
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              isAntiAlias: true),
+        ),
+        clipBehavior: Clip.antiAlias,
+        alignment: Alignment.bottomCenter,
+        child: Text('${widget.user.bio}',
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                shadows: const <Shadow>[
+                  Shadow(
+                    blurRadius: 5.0,
+                    color: Color.fromARGB(10, 0, 0, 0),
+                  ),
+                ])));
+  }
+
+  Widget _userPhoto(String url) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: CachedNetworkImageProvider(url),
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+            isAntiAlias: true),
+      ),
+    );
+  }
+}
