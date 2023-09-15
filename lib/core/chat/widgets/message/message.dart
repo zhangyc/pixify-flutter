23fb2c0385e5d890c884bf2790e51f6cca38522c
zongyanfu <zongyanfu@outlook.com>
2023-09-15 15:40:50 +0800

    d

--- lib/core/chat/widgets/message/message.dart
+++ lib/core/chat/widgets/message/message.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sona/core/chat/models/message.dart';
 import 'package:sona/core/chat/widgets/message/message_from_me.dart';
 import 'package:sona/core/chat/widgets/message/message_from_other.dart';
 import 'package:sona/core/chat/widgets/message/time.dart';
 
+import '../../../../utils/dialog/input.dart';
 import 'local_pending_message_from_me.dart';
 
 class MessageWidget extends StatelessWidget {
@@ -32,30 +35,41 @@ class MessageWidget extends StatelessWidget {
       _message = MessageFromOther(message: message);
     }
 
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: fromMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: [
-          Visibility(
-            visible: prevMessage == null || prevMessage!.time.add(const Duration(minutes: 5)).isBefore(message.time),
-            child: MessageTime(time: message.time)
-          ),
-          Container(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.75
+    return GestureDetector(
+      onLongPress: () async {
+        final action = await showRadioFieldDialog(context: context, options: {'Copy': 'copy', 'Delete': 'delete'}, dismissible: true);
+        if (action == 'copy') {
+          Clipboard.setData(ClipboardData(text: message.content));
+          Fluttertoast.showToast(msg: 'Message has been copied to Clipboard');
+        } else if (action == 'delete') {
+          Fluttertoast.showToast(msg: 'todo');
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: fromMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            Visibility(
+              visible: prevMessage == null || prevMessage!.time.add(const Duration(minutes: 5)).isBefore(message.time),
+              child: MessageTime(time: message.time)
             ),
-            margin: EdgeInsets.only(bottom: 12),
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            decoration: BoxDecoration(
-              color: fromMe ? Theme.of(context).primaryColor : Color(0xFFF9F9F9),
-              borderRadius: BorderRadius.circular(24)
-            ),
-            child: _message
-          )
-        ],
-  ),
+            Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.75
+              ),
+              margin: EdgeInsets.only(bottom: 12),
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              decoration: BoxDecoration(
+                color: fromMe ? Theme.of(context).primaryColor : Color(0xFFF9F9F9),
+                borderRadius: BorderRadius.circular(24)
+              ),
+              child: _message
+            )
+          ],
+        ),
+      ),
     );
   }
 }
