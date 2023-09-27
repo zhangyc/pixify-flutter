36b90c500525e095cedf59d116d0dc1a771d2d94
zongyanfu <zongyanfu@outlook.com>
2023-09-27 19:28:14 +0800

    analytics

--- lib/setting/screens/setting.dart
+++ lib/setting/screens/setting.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sona/account/providers/profile.dart';
 import 'package:sona/account/services/info.dart';
 import 'package:sona/common/widgets/button/colored.dart';
-import 'package:sona/common/widgets/image/user_avatar.dart';
 import 'package:sona/core/providers/token.dart';
 import 'package:sona/setting/screens/about.dart';
 import 'package:sona/utils/global/global.dart';
 
-import '../../common/widgets/webview.dart';
-import '../../core/match/util/http_util.dart';
 import '../../utils/dialog/input.dart';
 
 class SettingScreen extends StatefulHookConsumerWidget {
@@ -31,87 +27,55 @@ class _SettingScreen extends ConsumerState<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.back),
+          onPressed: Navigator.of(context).pop,
+        ),
+        centerTitle: true,
+        title: Text('Settings'),
+      ),
       body: CustomScrollView(
         slivers: [
+          SliverToBoxAdapter(child: SizedBox(height: 16)),
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                UserAvatar(url: ref.read(myProfileProvider)?.avatar??'',size: 80,)
-              ],
+            child: GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: _showNotificationSetting,
+              child: Row(
+                children: [
+                  SizedBox(width: 20),
+                  Icon(CupertinoIcons.bell),
+                  SizedBox(width: 8),
+                  Text('Notification', style: Theme.of(context).textTheme.bodyMedium),
+                  Expanded(child: Container()),
+                  // Text(openNotification ? 'on': 'off'),
+                  Icon(CupertinoIcons.forward),
+                  SizedBox(width: 20),
+                ],
+              ),
             ),
           ),
+          SliverToBoxAdapter(child: SizedBox(height: 24)),
           SliverToBoxAdapter(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Notification'),
-                CupertinoSwitch(
-                  value: openNotification,
-                  onChanged: (bool value) {
-                    //openNotification=value;
-                    post('/user/update',data: {
-                       'openPush':value
-                     }).then((resp){
-                       if(resp.isSuccess){
-                         openNotification=value;
-                         Fluttertoast.showToast(msg: 'Modification succeeded');
-                       }else {
-                         Fluttertoast.showToast(msg: 'Modification failed');
-                       }
-                     });
-                     setState(() {
-
-                     });
-                  },
-                ),
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: TextButton(
-              onPressed: () async{
-
+            child: GestureDetector(
+              behavior: HitTestBehavior.translucent,
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (c){
+                  return AboutPage();
+                }));
               },
-              child: Text('Account')
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: TextButton(
-              onPressed: () async{
-                var result=await showRadioFieldDialog(context: context, options:
-                {'Privacy Policy': '1',
-                  'Disclaimer': '2',
-                  'Terms and Conditions':'3'
-                });
-                if(result!=null){
-                  if(result=='1'){
-                    Navigator.push(context, MaterialPageRoute(builder: (c){
-                      return WebView(url: 'https://h5.sona.pinpon.fun/privacy-policy.html', title: 'Privacy policy');
-                    }));
-                  }else if(result=='2'){
-                    Navigator.push(context, MaterialPageRoute(builder: (c){
-                      return WebView(url: 'https://h5.sona.pinpon.fun/disclaimer.html', title: 'Disclaimer');
-                    }));
-                  }else if(result=='3'){
-                    Navigator.push(context, MaterialPageRoute(builder: (c){
-                      return WebView(url: 'https://h5.sona.pinpon.fun/terms-and-conditions.html', title: 'Terms and conditions');
-                    }));
-                  }
-                }
-              },
-              child: Text('About')
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              alignment: Alignment.center,
-              child: ColoredButton(
-                  onTap: _logout,
-                  borderColor: Colors.black,
-                  text: 'Logout'
+              child: Row(
+                children: [
+                  SizedBox(width: 20),
+                  Icon(CupertinoIcons.info),
+                  SizedBox(width: 8),
+                  Text('About', style: Theme.of(context).textTheme.bodyMedium),
+                  Expanded(child: Container()),
+                  // Text(openNotification ? 'on': 'off'),
+                  Icon(CupertinoIcons.forward),
+                  SizedBox(width: 20),
+                ],
               ),
             ),
           ),
@@ -119,9 +83,25 @@ class _SettingScreen extends ConsumerState<SettingScreen> {
             hasScrollBody: false,
             child: Align(
               alignment: Alignment.bottomCenter,
-              child: TextButton(
-                onPressed: _delete,
-                child: Text('Delete Account', style: TextStyle(color: Colors.grey))
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 180,
+                    child: ColoredButton(
+                        onTap: _logout,
+                        text: 'Logout',
+                      color: Colors.black,
+                      fontColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  TextButton(
+                    onPressed: _delete,
+                    child: Text('Delete Account', style: TextStyle(color: Colors.grey))
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
             ),
           )
@@ -130,9 +110,27 @@ class _SettingScreen extends ConsumerState<SettingScreen> {
     );
   }
 
+  void _showNotificationSetting() async {
+    final value = await showRadioFieldDialog(
+      context: context,
+      options: {
+        'on': true,
+        'off': false
+      },
+    );
+    if (value != null) toggleNotification(value);
+  }
+
+  Future toggleNotification(bool value) {
+    return dio.post('/user/update',data: {
+      'openPush': value
+    }).then((value){
+
+    });
+  }
+
   void _logout() {
     ref.read(tokenProvider.notifier).state = null;
-
   }
 
   Future _delete() async {
