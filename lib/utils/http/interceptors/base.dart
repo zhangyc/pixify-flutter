c53b8498cfe575eb68f165891407e2897b5f01f0
zongyanfu <zongyanfu@outlook.com>
2023-10-09 19:03:28 +0800

    bugfix

--- lib/utils/http/interceptors/base.dart
+++ lib/utils/http/interceptors/base.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:sona/account/providers/profile.dart';
+import 'package:sona/core/providers/token.dart';
 import 'package:sona/utils/global/global.dart';
+import 'package:sona/utils/timer/debounce.dart';
 
 
 class BaseInterceptor extends Interceptor {
@@ -16,21 +19,30 @@ class BaseInterceptor extends Interceptor {
         options.headers.addAll({HttpHeaders.contentTypeHeader: Headers.jsonContentType});
       }
     }
-    super.onRequest(options, handler);
+    return handler.next(options);
+    // super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     if (response.statusCode == 200) {
-      if (response.data['code'] == '10040') {
-        token = null;
-      } else if(response.data['code'] == '10030') {
-        token = null;
+      if (response.data['code'] == '10030' || response.data['code'] == '10040') {
+        _onTokenInvalid();
       }
       response.statusCode = int.parse(response.data['code']);
       response.data = response.data['data'];
     }
     return handler.next(response);
     //super.onResponse(response, handler);
+  }
+
+  void _onTokenInvalid() {
+    Debounce(duration: const Duration(seconds: 1)).run(_clearAndLogin);
+  }
+
+  void _clearAndLogin() {
+    token = null;
+    kvStore.remove(profileKey);
+    navigatorKey.currentState?.pushNamedAndRemoveUntil('login', (route) => false);
   }
 }
