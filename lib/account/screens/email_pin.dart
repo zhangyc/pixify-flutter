10de3e6513ee8f9a724b5f4220d7e29ea2ff157a
zongyanfu <zongyanfu@outlook.com>
2024-03-06 16:20:14 +0800

    bugfix

--- lib/account/screens/email_pin.dart
+++ lib/account/screens/email_pin.dart
@@ -205,6 +205,7 @@ class _EmailPinScreenState extends ConsumerState<EmailPinScreen> {
               _completeRequiredInfo();
               return;
             }
+            await Future.delayed(const Duration(milliseconds: 200));
             if (mounted) Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
           } else {
             Fluttertoast.showToast(msg: 'Failed to get profile, try again later');
